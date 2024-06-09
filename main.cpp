ae06dea524449e67005e5766d406de50152be6b4
bummings <bummings>
2024-06-09 17:18:38 -0400

    summarizes notes/overall documentation effort for class specifications, general structure, etc

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 /*
 //  Implement functions:
-//  
-//    Getters: 
-//      - Return values of `studentID`, `firstName`, `lastName`, 
+//
+//    Getters:
+//      - Return values of `studentID`, `firstName`, `lastName`,
 //        `emailAddress`, `age`, `daysInCourse`, `degreeProgram`
-//  
+//
 //    Setters:
-//      - Set values for `studentID`, `firstName`, `lastName`, 
+//      - Set values for `studentID`, `firstName`, `lastName`,
 //        `emailAddress`, `age`, `daysInCourse`, `degreeProgram`
-//  
+//
 //    Constructor:
 //      - Init all member variables using parameters
-//  
+//
 //    print():
 //      - Display student data in the specified format
 //
-//  
+//
 //
 //  Demonstrate program functionality:
 //
 //    Print information: course title, programming language, your student
 //    ID, your name
-//  
+//
 //    Create `classRoster` instance:  Instantiate the `Roster` class
 //
 //    Add students: add each studewnt from the `studentData` tabl to
@@ -32,3 +32,13 @@
 //
 //
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello, World!" << endl;
+  return 0;
+}
