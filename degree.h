5dcf2fee4608b3e0ea33661edc0f1b9619328d92
bummings <bummings>
2024-06-12 08:15:10 -0400

    adds student data table for parsing

--- degree.h
+++ degree.h
@@ -1,1 +1,12 @@
 // This will have a enum of with name DegreeProgram with the values: SECURITY, NETWORK, SOFTWARE
+
+#ifndef DEGREE_H
+#define DEGREE_H
+
+enum DegreeProgram {
+  SECURITY,
+  NETWORK,
+  SOFTWARE
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -34,11 +34,25 @@
 */
 
 #include <iostream>
+#include "degree.h"
 
 using namespace std;
 
 int main()
 {
-  cout << "Hello, World!" << endl;
+  DegreeProgram currentProgram = SECURITY;
+  cout << "OK OK" << endl;
+
+  const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Edd,Burke,hello@bummings.io,37,14,19,20,SOFTWARE" };
+
+  cout << studentData[3];
+
+  if (currentProgram == SECURITY) {
+    cout << "Security";
+  }
+  else {
+    cout << "Who knows man";
+  };
+
   return 0;
 }
