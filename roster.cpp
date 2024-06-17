96d6e772e4b65b2824ce9c54563a3ec6808e1eaa
edd bummings <bummings@lilNek.local>
2024-06-17 08:53:25 -0400

    continues with roster.cpp

--- roster.cpp
+++ roster.cpp
@@ -1,45 +1,17 @@
-/*
-//  Create Roster Class:
-//    - Define an array of pointers in `roster.h` and 
-//      `roster.cpp`
-//      This array of pointers will be called `classRosterArray` 
-//      that will hold  `studentDataTable` entries.
-//
-//  Populate `classRosterArray`:
-//    - Parse data from `studentDataTable`
-//    - Create Student objects and add them to `classRosterArray`
-//
-//  Implement functions:
-//    - add()
-//        Parameters: `studentId`, `firstName`, `lastName`,
-//        `emailAddress`, `age`, `daysInCourse1`, daysInCourse2`, 
-//        daysInCourse3`, `degreeProgram`
-//        Setup instance variables and update roster
-//    - remove() 
-//        Parameters: `studentId`
-//        Remove student by ID, print error if ID not found
-//
-//    - printAll()
-//        Loop through `classRosterArray` and call `print()`
-//        for each student.
-//    
-//    - printAverageDayInCourse()
-//        Parameter: `studentID`
-//        Calculate and print average days in courses for 
-//        the student
-//
-//    - printInvalidEmails()
-//        Validate email addresses and print invalid ones (Check
-//        for '@', '.', no spaces, etc.
-//
-//    - printByDegreeProgram()
-//        Parameter: `degreeProgram`
-//        Print student data for specified degree program.
-//
-//
-//   Define the constructor and destructor.
-//
-//
-//
-//
-*/
+#include <iostream>
+#include "roster.h"
+
+using namespace std;
+
+Roster::Roster() {}
+
+Roster::~Roster() {
+    for (int i = 0; i<= lastIndex; i++) {
+        delete classRosterArray;
+    }
+}
+
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
+    int daysInCourse[] = {daysInCourse1, daysInCourse2, daysInCourse3};
+    classRosterArray[++lastIndex] = new Student(studentID, firstName, lastName, emailAddress, age, daysInCourse, degreeProgram);
+}
