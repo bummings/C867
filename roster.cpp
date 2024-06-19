7e4e461bc3f213741e960b2e882b403d49b33d2a
bummings <hello@bummings.io>
2024-06-19 16:46:44 -0400

    adds a ton of logic to roster header and class

--- roster.cpp
+++ roster.cpp
@@ -5,26 +5,38 @@ using namespace std;
 
 Roster::Roster() {}
 
-Roster::~Roster()
-{
-    for (int i = 0; i <= lastIndex; i++)
-    {
+Roster::~Roster() {
+    for (int i = 0; i <= lastIndex; i++) {
         delete classRosterArray;
     }
 }
 
-void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram)
-{
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
     int daysInCourse[] = {daysInCourse1, daysInCourse2, daysInCourse3};
     classRosterArray[++lastIndex] = new Student(studentID, firstName, lastName, emailAddress, age, daysInCourse, degreeProgram);
 }
 
-void Roster::remove(string StudentID)
-{
+void Roster::remove(string StudentID) {
     bool found = false;
-    for (int i = 0; i <= lastIndex; ++i)
-    {
-        if (classRosterArray[i]->getStudentID() == StudentID)
-            ;
+    for (int i = 0; i <= lastIndex; ++i) {
+        if (classRosterArray[i]->getStudentID() == StudentID) {
+            delete classRosterArray[i];
+            classRosterArray[i] = classRosterArray[lastIndex];
+            classRosterArray[lastIndex] = nullptr;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        cout << "Cannot find student with the ID: " << StudentID; 
     }
 }
+
+void Roster::printAll() const {
+    for (int i = 0; i <= lastIndex; ++i) {
+        classRosterArray[i]->print();
+    }
+}
+
+// remaining methods printAverageDaysInCourse, printInvalidEmails, printByDegreeProgram
+
--- roster.h
+++ roster.h
@@ -1,8 +1,6 @@
 #ifndef ROSTER_H
 #define ROSTER_H
 
-using namespace std;
-
 #include "student.h"
 
 class Roster  {
@@ -11,20 +9,15 @@ class Roster  {
         ~Roster();  // destructor
 
         void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
-
         void remove(string studentID);
-
         void printAll() const;
-
         void printAverageDaysInCourse(string studentID) const;
-
         void printInvalidEmails() const;
-
         void printByDegreeProgram(DegreeProgram degreeProgram) const;
 
     private:
-        Student* classRosterArray[5];
-        int lastIndex = -1;
+        Student* classRosterArray[5]; // this is an array of pointers to Student objects
+        int lastIndex = -1; // index of last indexed student
 };
 
 #endif
